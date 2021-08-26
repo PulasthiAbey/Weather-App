2e4b07529a46901ced49b228b24b595e272ae158
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-08-26 21:30:47 +0530

    ..

---
+++ wether-app/src/components/home_page.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+
+import stylesComponent from "../tools/landingStyles";
+
+const home_page = () => {
+  return (
+    <ScrollView>
+      {/* View of the API */}
+      <View></View>
+    </ScrollView>
+  );
+};
+
+export default home_page;
