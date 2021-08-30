3ba73c1433e3c8b0a12122c2fdd2d5674d5cb6a9
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-08-30 20:02:39 +0530

    date & time fetched

--- weather-app/components/date_time.js
+++ weather-app/components/date_time.js
@@ -1,6 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { View, Text } from "react-native";
 
+// days & months array
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
 // weather item component to retrieve the data
 const WeatherItem = ({ title, value, unit }) => {
   return (
@@ -14,22 +40,45 @@ const WeatherItem = ({ title, value, unit }) => {
   );
 };
 
-function date_time() {
+const date_time = () => {
   const [date, setDate] = useState("");
   const [time, setTime] = useState("");
 
-  useEffect(() => {}, []);
-  
+  useEffect(() => {
+    setInterval(() => {
+      const time = new Date();
+      const month = time.getMonth();
+      const date = time.getDate();
+      const day = time.getDay();
+      const hour = time.getHours();
+      const hoursIn12HourFormat = hour >= 13 ? hour % 12 : hour;
+      const minute = time.getMinutes();
+      const ampm = hour > 12 ? "PM" : "AM";
+
+      setTime(
+        (hoursIn12HourFormat < 10
+          ? "0" + hoursIn12HourFormat
+          : hoursIn12HourFormat) +
+          " : " +
+          (minute < 10 ? "0" + minute : minute) +
+          " " +
+          ampm
+      );
+
+      setDate(days[day] + ", " + date + " " + months[month]);
+    }, 1000);
+  }, []);
+
   return (
     //   containers for the date and time functions in the app
     <View style={styles.container}>
       {/* Date container */}
       <View>
         <View>
-          <Text style={styles.heading}>12.30 am</Text>
+          <Text style={styles.heading}>{time}</Text>
         </View>
         <View>
-          <Text style={styles.subHeading}>Monday, Aug 30</Text>
+          <Text style={styles.subHeading}>{date}</Text>
         </View>
         {/* weather item container */}
         <View style={styles.weatherItemContainer}>
@@ -47,7 +96,7 @@ function date_time() {
       </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
