d6fd3c3657bd8f929d2d73211c197cdd33720496
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-09-01 19:09:49 +0530

    API call initiated in redux

---
+++ weather-app/redux/actions.js
@@ -0,0 +1,46 @@
+export const GET_WEATHER = "GET_DATA";
+export const GET_LAT = "GET_LAT";
+export const GET_LON = "GET_LON";
+
+const API_KEY = "49cc8c821cd2aff9af04c9f98c36eb74";
+const API_URL =
+  "https://api.openweathermap.org/data/2.5/onecall?lat=${GET_LAT}&lon=${GET_LON}&exclude=hourly,minutely&units=metric&appid=${API_KEY}";
+
+export const getWeather = () => {
+  try {
+    return async (dispatch) => {
+      const results = await fetch(API_URL, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const json = await results.json();
+      if (json) {
+        dispatch({
+          type: GET_WEATHER,
+          payload: json,
+        });
+      } else {
+        console.log("Unable to fetch data from API");
+      }
+    };
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getLatitude = (latitude) => (dispatch) => {
+  dispatch({
+    type: GET_LAT,
+    payload: latitude,
+  });
+};
+
+export const getLongitude = (longitude) => (dispatch) => {
+  dispatch({
+    type: GET_LON,
+    payload: longitude,
+  });
+};
---
+++ weather-app/redux/reducers.js
@@ -0,0 +1,22 @@
+import { GET_LON, GET_LAT, GET_WEATHER } from "./actions";
+
+const initialState = {
+  latitude: "6.9271",
+  longitude: "79.8612",
+};
+
+function userReducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_LAT:
+      return { ...state, latitude: action.payload };
+
+    case GET_LON:
+      return { ...state, longitude: action.payload };
+
+    case GET_WEATHER:
+      return { ...state, weather: action.payload };
+
+    default:
+      return state;
+  }
+}
