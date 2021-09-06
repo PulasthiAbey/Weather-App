94ae8ca84005413643522b56cfc4f08b7894d420
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-09-06 14:42:12 +0530

    Completed

--- weather-app/App.js
+++ weather-app/App.js
@@ -1,82 +1,60 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useEffect, useState } from "react";
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
-import * as Location from "expo-location";
-import { Provider } from "react-redux";
-import { Store } from "./src/redux/store";
-import { getWeather, getLatitude, getLongitude } from "./src/redux/actions";
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Alert,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import { useDispatch, useSelector } from "react-redux";
-import DateTime from "./src/components/date_time";
-import WeatherScroll from "./src/components/weather_scroll";
 
-// API KEY
-// const API_KEY = "49cc8c821cd2aff9af04c9f98c36eb74";
+import { getWeather } from "./src/store/actions/weatherActions";
+import Form from "./src/store/components/Form";
+import Weather from "./src/store/components/Weather";
 
-const img = require("./assets/image.png");
-
-export default function App() {
-  // const [data, setData] = useState({});
-  const { latitude, longitude, weather } = useSelector(
-    (state) => state.apiCallReducer
-  );
+const App = () => {
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
+  const { data, error } = useSelector((state) => state.weather);
 
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        // fetchDataFromApi();
-        dispatch(getWeather());
-        return;
-      }
+  const searchSubmitHandler = () => {
+    if (search === "") {
+      return Alert.alert("Validation", "City name is required!", [
+        { text: "OK" },
+      ]);
+    }
 
-      let location = await Location.getCurrentPositionAsync({});
-      latitude = location.coords.latitude;
-      longitude = location.coords.longitude;
-      dispatch(getLongitude(longitude));
-      dispatch(getLatitude(latitude));
-    })();
-  }, []);
+    setLoading(true);
+    dispatch(
+      getWeather(
+        search,
+        () => setLoading(false),
+        () => setLoading(false)
+      )
+    );
+    setSearch("");
+    Keyboard.dismiss();
+  };
 
-  // const fetchDataFromApi = (latitude, longitude) => {
-  //   if (latitude && longitude) {
-  //     fetch(
-  //       `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`
-  //     )
-  //       .then((res) => res.json())
-  //       .then((data) => {
-  //         // console.log(data)
-  //         setData(data);
-  //       });
-  //   }
-  // };
   return (
-    <Provider store={Store}>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <View style={styles.container}>
-        <ImageBackground source={img} style={styles.imageStyle}>
-          <DateTime
-          // current={weather.current}
-          // lat={weather.lat}
-          // lon={weather.lon}
-          // timezone={weather.timezone}
-          />
-          <WeatherScroll
-          // weatherData={data.daily}
-          />
-        </ImageBackground>
+        <Form
+          search={search}
+          onSetSearch={setSearch}
+          onSubmit={searchSubmitHandler}
+        />
+        <Weather loading={loading} data={data} error={error} />
       </View>
-    </Provider>
+    </TouchableWithoutFeedback>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-
-  imageStyle: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center",
-  },
 });
+
+export default App;
--- weather-app/src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import { apiCallReducer, settingUpDateReducer } from "./reducers";
-
-const rootReducer = combineReducers({ apiCallReducer, settingUpDateReducer });
-
-export const Store = createStore(rootReducer, applyMiddleware(thunk));
